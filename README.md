b390606927b15c2ff5ebc64ccfecd14049ecae41
Alexander McPhail <mcphail.alex@unknown6c5ab077fe4b.attlocal.net>
2022-10-03 18:47:27 -0500

    working server and routes

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # view-finder
 
-<<<<<<< HEAD
- 
- 
-=======
+
 
 
 We ran into an issue with npm install, and ran the following commands to get past it:
@@ -19,4 +16,4 @@ nvm ls
 npm --version
 
 npm install
->>>>>>> 3593acd24e33ad43ab7e27fe7980b922111df73e
+
--- server/server.js
+++ server/server.js
@@ -1,1 +1,46 @@
 //testing github stuff
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+const apiRouter = require('./routes/api.js')
+
+
+app.use(express.json());
+
+app.use('/build', express.static(path.join(__dirname, '../build')));
+// route handler to respond with main app
+app.get("/",
+    (req, res) => {
+
+        return res.status(200).sendFile(path.join(__dirname, "../index.html"));
+    })
+
+
+app.use('/api', apiRouter);
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.sendStatus(404));
+
+app.use((error, req, res, next) => {
+    const defaultErr = {
+        log: "Express error handler caught unknown middleware error",
+        status: 400,
+        message: { err: "An error occurred" },
+    };
+    console.log(error);
+    const errorObj = Object.assign(defaultErr, error);
+    console.log("error log " + errorObj.log)
+    res.locals.message = errorObj.message;
+    return res.status(errorObj.status).send(eval(errorObj.message));
+    // return res.status(errorObj.status).send(res.locals.message);
+});
+
+/**
+* start server
+*/
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
