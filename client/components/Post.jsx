77aa87b47ebdf71522a4dba60c42481f7232cc26
hbernie <hbern00@gmail.com>
2022-10-05 18:46:51 -0700

    fixing categories as number

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -2,21 +2,20 @@ import React, { useState, useEffect } from 'react'
 
 const Post = props => {
 	let locCard = [];
-	let classypoo;
-	if (props.savedLocations.category === 'photospot'){
+	if (props.savedLocations.category === 1){
 		locCard.push(<div className='photospot'><div>{props.savedLocations.name}</div>
 		<div>{props.savedLocations.street_address}</div>
 		<div>{props.savedLocations.city}, {props.savedLocations.state} {props.savedLocations.zip_code}</div>
 		<div>{props.savedLocations.caption}</div></div>)
 	}
-	else if (props.savedLocations.category === "food"){
+	else if (props.savedLocations.category === 2){
 		locCard.push(<div className='food'><div>{props.savedLocations.name}</div>
 		<div>{props.savedLocations.street_address}</div>
 		<div>{props.savedLocations.city}, {props.savedLocations.state} {props.savedLocations.zip_code}</div>
 		<div>{props.savedLocations.caption}</div></div>)
 	}
 
-	else if (props.savedLocations.category === "hiking"){
+	else if (props.savedLocations.category === 3){
 		locCard.push(<div className='hiking'><div>{props.savedLocations.name}</div>
 		<div>{props.savedLocations.street_address}</div>
 		<div>{props.savedLocations.city}, {props.savedLocations.state} {props.savedLocations.zip_code}</div>
