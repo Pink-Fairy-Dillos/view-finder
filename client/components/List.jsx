4e0d4160e3123924c1ccabb028f01f903d3aa33e
Kristen Abby Cendana <kristencendana@gmail.com>
2022-10-05 17:17:35 -0700

    added in files List, Map, and styles to add scroll features and add filter features

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -2,37 +2,77 @@ import React, { Component, useState } from 'react';
 import Post from "./Post.jsx"
 function List (props) {
 
+  const [filter, setFilter] = useState(false);
   // This is the array of saved locations pulled from the database
   const savedLocations = props.savedLocations;
 
-  // This is the current selected pin on the map
-  // const [selected, setSelected] = useState({});
+// This function takes values from input fields and updates the userData piece of state,
+const handleFilter = (e, property) => {
+      let updatedValue = {};
+      const publicVal = document.getElementById(property); 
+      if(publicVal.checked){
+          updatedValue = {[property]: false};
+      }
+      else{
+          updatedValue = {[property]: true};
+      }
+  }
 
-  // const onSelect = item => {
-  //   setSelected(item);
-  // }
+const applyFilter = () => {
 
-  // useEffect(() => {
-  //   fetch('api/locations')
-  //     .then(res => res.json())
-  //     .then((locations) => {
-  //       if (!Array.isArray(locations)) locations = [];
-  //       setSavedLocations(locations);
-  //     })
-  // })
-
+  // re render the page...
+  // only rerender list and below
+}
   
 	let list = [];
     for (let i = 0; i< savedLocations.length; i++){
-      list.push(<Post savedLocations= {savedLocations[i]} />)
+      if (!filter){
+        list.push(<Post savedLocations= {savedLocations[i]} />)
+      }
     }
 
   return (
     <div id='listContainer'>
-		<h1>Places:</h1>
-		<div id='postContainer'>
-      {list}
-		</div>
+      <h1>Places:</h1>
+      <label>PhotoSpot</label>
+          <input 
+                id="photospot-checkbox"
+                type="checkbox"
+                name="photospot"
+                // defaultValue={}
+                onChange={(e) => handleFilter(e, "photospot-checkbox")}></input>
+
+      <label>Food</label>
+              <input 
+                id="food-checkbox"
+                type="checkbox"
+                name="food"
+                // defaultValue={}
+                onChange={(e) => handleFilter(e, "food-checkbox")}></input>
+      <label>Hiking</label>
+                <input 
+                id="hiking-checkbox"
+                type="checkbox"
+                name="hiking"
+                // defaultValue={}
+                onChange={(e) => handleFilter(e, "hiking-checkbox")}></input>
+
+                <label>Other</label>
+                  <input 
+                  id="other-checkbox"
+                  type="checkbox"
+                  name="other"
+                  // defaultValue={}
+                  onChange={(e) => handleChange(e, "other-checkbox")}></input>
+                  {/* <input type='submit'>Apply Filter</input> */}
+                  <button onClick={applyFilter}>Apply Filter</button>
+
+      <div id='scrollContainer'>
+        <div id='postContainer'>
+          {list}
+        </div>
+      </div>
+      
     </div>
   )
 }
